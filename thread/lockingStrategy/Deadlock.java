021d1d59f4c590dc21715e4288ceb8a7d3002aec
Rishi Singh <rishi.singh@maxlifeinsurance.com>
2024-05-01 02:44:35 +0530

    explored deadlock and solutions

---
+++ thread/lockingStrategy/Deadlock.java
@@ -0,0 +1,89 @@
+package thread.lockingStrategy;
+
+import java.util.Random;
+
+public class Deadlock {
+    public static void main(String[] args) {
+        Intersection intersection = new Intersection();
+        Thread trainAThread = new Thread(new TrainA(intersection));
+        Thread trainBThread = new Thread(new TrainB(intersection));
+
+        trainAThread.start();
+        trainBThread.start();
+    }
+
+    private static class TrainB implements Runnable{
+        private  Intersection intersection;
+        private Random random = new Random();
+
+        public TrainB(Intersection intersection){
+            this.intersection = intersection;
+        }
+
+        @Override
+        public void run(){
+            while(true){
+                long sleepingTime = random.nextInt(5);
+                try {
+                    Thread.sleep(sleepingTime);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                intersection.takeRoadB();
+            }
+        }
+    }
+    private static class TrainA implements Runnable{
+        private  Intersection intersection;
+        private Random random = new Random();
+
+        public TrainA(Intersection intersection){
+            this.intersection = intersection;
+        }
+
+        @Override
+        public void run(){
+            while(true){
+                long sleepingTime = random.nextInt(5);
+                try {
+                    Thread.sleep(sleepingTime);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                intersection.takeRoadA();
+            }
+        }
+    }
+    public static class Intersection {
+        private Object roadA = new Object();
+        private Object roadB = new Object();
+
+        public void takeRoadA() {
+            synchronized (roadA) {
+                System.out.println("Road A is lock by thread: " + Thread.currentThread().getName());
+                synchronized (roadB) {
+                    System.out.println("Train is passing through road A");
+                    try {
+                        Thread.sleep(1);
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+            }
+        }
+
+        public void takeRoadB() {
+            synchronized (roadB) {
+                System.out.println("Road B is lock by thread: " + Thread.currentThread().getName());
+                synchronized (roadA) {
+                    System.out.println("Train is passing through road B");
+                    try {
+                        Thread.sleep(1);
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+            }
+        }
+    }
+}
